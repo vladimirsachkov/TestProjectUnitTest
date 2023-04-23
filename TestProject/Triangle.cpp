188a5b6d9019cd9648f97d14a7921ad40b8641a7
alexa <alexanderredar1@gmail.com>
2023-04-24 00:05:05 +0300

    Added comments

--- TestProject/Triangle.cpp
+++ TestProject/Triangle.cpp
@@ -1,30 +1,48 @@
 #include "Triangle.h"
 #include <string>
+
+//ENG: Autor:Sachkov Vladimir Igorevich
+//РУС: Автор:Сачков Владимир Игоревич
+
+//ENG:Class Triangle 
+//РУС:Класс треугольника
 class Triangle
 {
 
-private:double a, b, c;
+//ENG: Sides of a triangle 
+//РУС: Стороны треугольника
+private: double a, b, c;
 
 public:
 
+	//ENG:Constructor 
+	//РУС:Конструктор
 	Triangle(double valueA, double valueB, double valueC) {
 		a = valueA;
 		b = valueB;
 		c = valueC;
 	}
 
+	//ENG:Getting the perimeter
+	//РУС:Получение периметра
 	double getP() {
 		return a+b+c;
 	};
 
+	//ENG:Getting the first side of a triangle 
+	//РУС:Получение первой стороны треугольника
 	double getA() {
 		return a;
 	}
 
+	//ENG:Getting the secind side of a triangle 
+	//РУС:Получение второй стороны треугольника
 	double getB() {
 		return b;
 	}
 
+	//ENG:Getting the third side of a triangle 
+	//РУС:Получение второй стороны треугольника
 	double getC() {
 		return c;
 	}
--- TriangleTest/TriangleTest.cpp
+++ TriangleTest/TriangleTest.cpp
@@ -9,10 +9,17 @@ namespace TriangleTest
 {
 	TEST_CLASS(TriangleTest)
 	{
+
+		//ENG:Getting the perimeter
+	//РУС:Получение периметра
 		
 		TEST_METHOD(GetPTest)
 		{
 			Triangle triangle = Triangle(23.00, 34.00, 34.00);
+			//    --ENG:Get the perimeter and Expected value
+			//    --RUS:Получение периметра и Ожидаемое значение
+
+			//23+34+34 = 91
 			Assert::IsTrue(triangle.getP() == 91.00);
 		}
 	};
